a7144c3b187fc640449f2f882aefd7f08ddde904
Vaibhav Bali <vibsbali@gmail.com>
2016-09-15 08:04:54 +1000

    Used productService in productDetailController

--- Acme.ProductManagement/app/products/productDetailController.js
+++ Acme.ProductManagement/app/products/productDetailController.js
@@ -2,12 +2,13 @@
 (function () {
     angular.module("productManagement")
         //product is injected via resolve property via resolve in app.js
-        .controller("ProductDetailController",["product", function(product) {
+        .controller("ProductDetailController",["product","productService", function(product, productService) {
             var vm = this;
 
             vm.product = product;
 
             vm.title = "Product Detail : " + vm.product.productName;
+            vm.marginPercent = productService.calculateMarginPercent(vm.product.price, vm.product.cost);
 
             //vm.product.tags is an array and calling to list will 
             //conver the array to a comma separated list
