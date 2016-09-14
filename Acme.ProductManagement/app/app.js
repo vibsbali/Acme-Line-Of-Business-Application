98aa739045ea0832eaf54e8254d9c85e9f9db00f
Vaibhav Bali <vibsbali@gmail.com>
2016-09-14 15:22:40 +1000

    Added three new states using $stateProvider and $urlRouterProvider

--- Acme.ProductManagement/app/app.js
+++ Acme.ProductManagement/app/app.js
@@ -1,4 +1,27 @@
 ﻿"use strict";
 (function () {
-    angular.module("productManagement", ["common.services", "productResourceMock", "ui.router"]);
+    var app = angular.module("productManagement", ["common.services", "productResourceMock", "ui.router"]);
+
+    app.config([
+        "$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/welcomeView.html"
+                })
+                .state("productList", {
+                    url: "/products",
+                    "templateUrl": "app/products/productListView.html",
+                    controller: "ProductListController as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit:productId",
+                    "templateUrl": "app/products/productEditView.html",
+                    controller: "ProductEditController as vm"
+                });
+        }
+    ]);
 }());
